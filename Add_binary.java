a79fe40f6e18e784ddf2238766b67156f27187e3
pranavvangavety <vangavetypranav@gmail.com>
2025-02-21 12:07:06 -0500

    21/02/2025

---
+++ Add_binary.java
@@ -0,0 +1,30 @@
+public class Add_binary {
+    public static String addBinary(String a, String b){
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1, j = b.length() -1;
+        int carry = 0;
+
+        while( i >= 0 || j >=0 || carry > 0){
+            int sum = carry;
+
+            if(i >= 0){
+                sum+= a.charAt(i) - '0';
+                i--;
+            }
+            if(j>=0){
+                sum+=b.charAt(j) - '0';
+                j--;
+            }
+
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        return sb.reverse().toString();
+    }
+
+    public static void main(String[] args){
+        String a = "1", b = "1";
+        System.out.println(addBinary(a,b));
+    }
+}
