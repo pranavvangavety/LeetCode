82b9e9a7ac92d8b82e5ff4bba93c8a57b5f081f4
pranavvangavety <vangavetypranav@gmail.com>
2025-03-12 16:28:38 -0400

    12/03/2025

--- Excel_Sheet_Column_Number.java
+++ Excel_Sheet_Column_Number.java
@@ -1,2 +1,15 @@
 public class Excel_Sheet_Column_Number {
+    public int titleToNumber(String columnTitle) {
+        int result = 0;
+        for (char c : columnTitle.toCharArray()) {
+            result = result * 26 + (c - 'A' + 1);
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Excel_Sheet_Column_Number solution = new Excel_Sheet_Column_Number();
+
+        System.out.println(solution.titleToNumber("A"));
+
+    }
 }
