0f9008b9819fddf577f97fa0567ad6400663e217
pranavvangavety <vangavetypranav@gmail.com>
2025-03-17 13:12:02 -0400

    17/03/2025 - Re

--- Search_insert_position.java
+++ Search_insert_position.java
@@ -2,20 +2,21 @@ public class Search_insert_position {
 
     public static int searchInsert(int[] nums, int target){
         int low = 0;
-        int high = nums.length - 1;
+        int high = nums.length -1;
 
         while(low<=high){
 
-            int mid = low + (high-low) / 2; //high-low to prevent overflow
+            int mid = low + (high-low) / 2;
 
             if(nums[mid] == target){
                 return mid;
-            } else if (nums[mid] > target) {
-                high = mid - 1;
-            }else{
+            }else if(nums[mid] < target){
                 low = mid + 1;
+            }else{
+                high = mid - 1;
             }
         }
+
         return low;
     }
     public static void main(String[] args){
