4272a1258427b6daf949c8b7da94074e55926e79
pranavvangavety <vangavetypranav@gmail.com>
2025-03-18 08:28:22 -0400

    18/03/2025 - Re

--- Add_binary.java
+++ Add_binary.java
@@ -1,26 +1,26 @@
 public class Add_binary {
     public static String addBinary(String a, String b){
-        StringBuilder sb = new StringBuilder();
-        int i = a.length() - 1, j = b.length() -1;
+        StringBuilder x = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
         int carry = 0;
 
-        while( i >= 0 || j >=0 || carry > 0){
+        while(i >=0 || j>=0 || carry>0){
             int sum = carry;
 
-            if(i >= 0){
+            if(i>=0){
                 sum+= a.charAt(i) - '0';
                 i--;
             }
             if(j>=0){
-                sum+=b.charAt(j) - '0';
+                sum+= b.charAt(j) - '0';
                 j--;
             }
-
-            sb.append(sum % 2);
-            carry = sum / 2;
+            x.append(sum % 2);
+            carry= sum/2;
         }
 
-        return sb.reverse().toString();
+        return x.reverse().toString();
     }
 
     public static void main(String[] args){
