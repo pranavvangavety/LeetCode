14838ad90e0fceeb4c3a2b22ef673d414ee5255f
pranavvangavety <vangavetypranav@gmail.com>
2025-03-19 17:19:18 -0400

    19/03/2025 - Re

--- Climbing_stairs.java
+++ Climbing_stairs.java
@@ -1,18 +1,21 @@
 public class Climbing_stairs {
 
     public static int climbStairs(int n) {
-        int  twostepsbefore  = 1; // First step
-        int onestepbefore = 2; // Second step
+        int onestepbefore = 2;
+        int twostepsbefore = 1;
         int current = 0;
 
-        if(n == 1 || n == 2){ // base case for f(1) & f(2)
+        if(n==1||n==2){
             return n;
         }
-        for(int i = 3;i<=n;i++){
+
+        for(int i = 3;i<=n; i++){
             current = onestepbefore + twostepsbefore;
+
             twostepsbefore = onestepbefore;
             onestepbefore = current;
         }
+
         return current;
 
     }
