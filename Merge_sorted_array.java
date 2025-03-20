b7fb9a10a8dd4cc7a9ac9c35a0ea312eeb5426df
pranavvangavety <vangavetypranav@gmail.com>
2025-03-20 10:44:58 -0400

    20/03/2025 - Re

--- Merge_sorted_array.java
+++ Merge_sorted_array.java
@@ -3,28 +3,26 @@ import java.util.Arrays;
 public class Merge_sorted_array {
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
         int p1 = m-1;
-        int p2 = n-1;
-        int p = m+n-1;
+        int p2 = n -1;
+        int p = m+n -1;
 
         while(p1>=0 && p2>=0){
-
-            if(nums1[p1]>nums2[p2]){
+            if(nums1[p1] > nums2[p2]){
                 nums1[p] = nums1[p1];
                 p1--;
             }else{
                 nums1[p] = nums2[p2];
                 p2--;
             }
-
             p--;
         }
-
         while(p2>=0){
             nums1[p] = nums2[p2];
             p2--;
             p--;
         }
     }
+
     public static void main(String[] args){
         int[] nums1 = {1,2,3,0,0,0};
         int[] nums2 = {4,5,6};
