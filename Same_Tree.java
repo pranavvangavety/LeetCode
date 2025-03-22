dfbfbee66c55ef54b5a38b73d418d9cfe039c79f
pranavvangavety <vangavetypranav@gmail.com>
2025-03-22 14:49:51 -0400

    22/03/2025 - Re

--- Same_Tree.java
+++ Same_Tree.java
@@ -14,21 +14,17 @@ public class Same_Tree {
      }
 
     public static boolean isSameTree(TreeNode p, TreeNode q) {
-         if(p == null && q == null){
-             return true;
-         } else if (p == null || q == null) {
-             return false;
-         }else{
-             return(p.val == q.val)
-                     && isSameTree(p.left,q.left)
-                     && isSameTree(p.right,q.right);
-//             /* can also be written as:*/
-//             if(p.val == q.val&& isSameTree(p.left,q.left) && isSameTree(p.right,q.right)){
-//                 return true;
-//             }else{
-//                 return false;
-//             }
-         }
+        if( p == null && q == null){
+            return true;
+        }else if( p == null || q == null){
+            return false;
+        }else{
+            if(p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)){
+                return true;
+            }else{
+                return false;
+            }
+        }
     }
 
     public static void main(String[] args){
