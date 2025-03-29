f85fe158bc6f4cd0f84a9479768e3082f43f5915
pranavvangavety <vangavetypranav@gmail.com>
2025-03-29 12:42:44 -0400

    29/03/2025 - Re

--- Maximum_depth_of_binary_tree.java
+++ Maximum_depth_of_binary_tree.java
@@ -4,18 +4,16 @@ public class Maximum_depth_of_binary_tree {
          int val;
          TreeNode left;
          TreeNode right;
-         TreeNode() {}
+//         TreeNode() {}
          TreeNode(int val) { this.val = val; }
-         TreeNode(int val, TreeNode left, TreeNode right) {
-             this.val = val;
-             this.left = left;
-             this.right = right;
-         }
+//
      }
 
 
     public static int maxDepth(TreeNode root) {
-        if(root == null) return 0;
+        if(root == null){
+            return 0;
+        }
         int leftdepth = maxDepth(root.left);
         int rightdepth = maxDepth(root.right);
         return(1+Math.max(leftdepth, rightdepth));
