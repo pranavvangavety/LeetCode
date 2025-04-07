8d92d7d8474792e0ebdcc14abdea37ffe67f4ade
pranavvangavety <vangavetypranav@gmail.com>
2025-04-07 11:29:07 -0400

    07/04/2025 - Re

--- Minimum_depth_of_binary_tree.java
+++ Minimum_depth_of_binary_tree.java
@@ -4,32 +4,30 @@ public class Minimum_depth_of_binary_tree {
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode() {}
         TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
-            this.val = val;
-            this.left = left;
-            this.right = right;
-        }
+
     }
 
     public static int minDepth(TreeNode root) {
         if(root == null){
             return 0;
         }
-        if (root.left == null && root.right == null) {
+        else if(root.left == null && root.right == null){
             return 1;
         }
-        if(root.left == null){
-            return 1+ minDepth(root.right);
+        else if(root.left == null){
+            return 1 + minDepth(root.right);
         }
-        if(root.right == null){
-            return 1+ minDepth(root.left);
+        else if(root.right == null){
+            return 1 + minDepth(root.left);
         }
 
-        return 1+ Math.min(minDepth(root.left), minDepth(root.right));
+        int leftdepth = minDepth(root.left);
+        int rightdepth = minDepth(root.right);
 
+        return 1 + Math.min(leftdepth, rightdepth);
     }
+
 
     public static void main(String[] args){
         TreeNode root = new TreeNode(1);
